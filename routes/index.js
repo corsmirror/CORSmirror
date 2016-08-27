da53118880ba47ed41cc7da9461c12b5deabdda3
Mark <mark@remarkablemark.org>
2016-08-27 18:35:09 -0400

    Fix linting errors
    - Tidy up app files based on linter configuration.
    - Update ESLint rules.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * GET home page.
+ */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'CORSMirror' });
 });
 
+/**
+ * Export router.
+ */
 module.exports = router;
