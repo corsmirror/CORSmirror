29c30854a4159b82da874e0d9b2aeac7e5c28478
Mark <mark@remarkablemark.org>
2016-08-27 20:40:06 -0400

    Add the request logic to `/v1/cors`
    If the query paramter `url` is present, then make a request with
    the method and respond with the status code, headers, and body.
    
    https://github.com/request/request

--- routes/v1.js
+++ routes/v1.js
@@ -3,8 +3,8 @@
 /**
  * Module dependencies.
  */
-var express = require('express');
-var router = express.Router();
+var router = require('express').Router();
+var request = require('request');
 
 /**
  * CORS mirror API.
@@ -13,8 +13,26 @@ var router = express.Router();
 router.all('/cors', function(req, res, next) {
     var url = req.query.url;
 
+    // check query parameter `url`
+    // Express will decode the encoded value
     if (url) {
-        res.send(req.method + ': ' + req.query.url);
+        var options = {
+            method: req.method,
+            url: url
+        };
+
+        // make a request to the url
+        // and respond with the status, headers, and body
+        request(options, function(error, response, body) {
+            if (error) {
+                return next(error);
+            }
+
+            res.status(response.statusCode);
+            res.set(response.headers);
+            res.send(body);
+        });
+
         // return or else you will get the following:
         // Error: Can't set headers after they are sent.
         return;
