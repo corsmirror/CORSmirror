ff1605e7968e7ed73de625f5a99b8620a9a96542
Mark <mark@remarkablemark.org>
2016-09-05 18:23:57 -0400

    Render `README.md` as HTML on the index page
    Use `marked` to convert markdown to HTML:
    https://www.npmjs.com/package/marked

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,22 @@
  */
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var path = require('path');
+var marked = require('marked');
+
+// convert README from markdown to HTML
+var readmeMD = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf8');
+var readmeHTML = marked(readmeMD);
 
 /**
  * GET home page.
  */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'CORSmirror' });
+    res.render('index', {
+        title: 'CORSmirror',
+        readme: readmeHTML
+    });
 });
 
 /**
