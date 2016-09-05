319b314b50baa4b3c57f85bc69befc0829c1f0fc
Mark <mark@remarkablemark.org>
2016-09-05 17:15:04 -0400

    Replace "jade" with "hbs" as the view engine

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
  * View engine.
  */
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 /**
  * Middleware.
