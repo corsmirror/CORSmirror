de1381d6638c31ea830d1cff80f6a9adce0a7ed8
Mark <mark@remarkablemark.org>
2016-09-05 21:35:03 -0400

    Highlight code blocks using `highlight.js` with `marked`
    - Highlight `js` code but don't highlight `sh` and unspecified
      languages
    - Add the GitHub color theme for `highlight.js` to the page
    - Update styling for <code>
    
    Documentation:
    - https://github.com/chjj/marked#highlight
    - https://highlightjs.org

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,18 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 var marked = require('marked');
+var highlight = require('highlight.js');
+
+// highlight markdown code blocks
+// https://github.com/chjj/marked#highlight
+marked.setOptions({
+    highlight: function(code, language) {
+        // do not highlight `sh` and unspecified languages
+        if (!/sh|^undefined$/.test(language)) {
+            return highlight.highlightAuto(code).value;
+        }
+    }
+});
 
 // convert README from markdown to HTML
 var readmeMD = fs.readFileSync(path.join(__dirname, '../README.md'), 'utf8');
