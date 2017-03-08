1e60c55d074a158c0624ea6a8f1354e484ba2df7
Mark <mark@remarkablemark.org>
2017-03-07 21:31:07 -0500

    Write test that checks that Google Analytics is rendered
    If environment variable `GOOGLE_ANALYTICS_ID` is present, then
    Google Analytics should be rendered. (The inverse case should
    hold as well.)

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@
 /**
  * Module dependencies.
  */
+var assert = require('assert');
 var supertest = require('supertest');
 var app = require('../app');
 var agent = supertest.agent(app);
@@ -13,11 +14,32 @@ var agent = supertest.agent(app);
 describe('default routes', function() {
 
     describe('home page', function() {
+        afterEach(function() {
+            delete process.env.GOOGLE_ANALYTICS_ID;
+        });
+
         it('responds with 200 and html', function(done) {
             agent
                 .get('/')
                 .expect(200)
                 .expect('Content-Type', /html/)
+                .expect(function(res) {
+                    assert(!/google-analytics/.test(res.text));
+                })
+                .end(done);
+        });
+
+        it('renders Google Analytics if ID is present', function(done) {
+            var gaId = 'UA-00000000-0';
+            process.env.GOOGLE_ANALYTICS_ID = gaId;
+            agent
+                .get('/')
+                .expect(200)
+                .expect('Content-Type', /html/)
+                .expect(function(res) {
+                    assert(/google-analytics/.test(res.text));
+                    assert(new RegExp(gaId).test(res.text));
+                })
                 .end(done);
         });
     });
