5c82fb69db9f4f6ee651540d1f2914050ab05c96
Mark <mark@remarkablemark.org>
2023-09-15 00:31:06 -0400

    refactor(routes): replace deprecated marked option highlight
    https://github.com/markedjs/marked/releases/tag/v8.0.0

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,26 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const { marked } = require('marked');
-const highlight = require('highlight.js');
+const { Marked } = require('marked');
+const { markedHighlight } = require('marked-highlight');
+const hljs = require('highlight.js');
 
-// highlight markdown code blocks
-// https://github.com/chjj/marked#highlight
-marked.setOptions({
-  highlight: (code, language) => {
-    if (language) {
-      return highlight.highlightAuto(code).value;
-    }
-  },
-});
+// https://www.npmjs.com/package/marked-highlight
+const marked = new Marked(
+  markedHighlight({
+    highlight(code, lang) {
+      const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+      return hljs.highlight(code, { language }).value;
+    },
+  })
+);
 
 // convert README from markdown to HTML
 const readmeMarkdown = fs.readFileSync(
   path.resolve(__dirname, '../README.md'),
   'utf8'
 );
-const readmeHTML = marked(readmeMarkdown);
+const readmeHTML = marked.parse(readmeMarkdown);
 
 /**
  * GET /
